eb4c33d1b39034ea3f235e7f7030f9e4abe8217b
wangfei <785509893@qq.com>
2021-01-25 11:12:14 +0800

    gate way

--- provider-service/src/main/java/com/wf/ProviderServiceApplication.java
+++ provider-service/src/main/java/com/wf/ProviderServiceApplication.java
@@ -2,10 +2,11 @@ package com.wf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient // 开启服务发现
+@EnableDiscoveryClient//开注册中心客户的自动配置
 public class ProviderServiceApplication {
 
 	public static void main(String[] args) {
